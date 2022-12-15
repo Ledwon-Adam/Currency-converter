60b79b17e70fa87192d5a7966cca26fc27387583
Adam Ledwon <ledwon.adam1989@gmail.com>
2022-12-15 11:44:45 +0100

    improve js code

--- js/script.js
+++ js/script.js
@@ -1,64 +1,68 @@
-let howMuchElement = document.querySelector(".js-howMuch");
-let choiceElement = document.querySelector(".js-choice");
-let numberElement = document.querySelector(".js-recalculate");
-let resultElement = document.querySelector(".js-result");
+{
 
-
-
-numberElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let choice = choiceElement.value;
-    let howMuch = howMuchElement.value;
-    let result;
-    let waluta;
-
-    switch (choice) {
-        case "PLN/EUR":
-            result = howMuch * 0.21;
-            waluta = "EUR";
-            break;
-
-        case "PLN/USD":
-            result = howMuch * 0.22;
-            waluta = "USD";
-            break;
-
-        case "PLN/GBP":
-            result = howMuch * 0.18;
-            waluta = "GBP";
-            break;
-
-        case "PLN/CHF":
-            result = howMuch * 0.21;
-            waluta = "CHF";
-            break;
-
-        case "EUR/PLN":
-            result = howMuch * 4.7;
-            waluta = "PLN";
-            break;
-
-        case "USD/PLN":
-            result = howMuch * 4.48;
-            waluta = "PLN";
-            break;
-
-        case "GBP/PLN":
-            result = howMuch * 5.45;
-            waluta = "PLN";
-            break;
-
-        case "CHF/PLN":
-            result = howMuch * 4.75;
-            waluta = "PLN";
-            break;
+    const calculateResult = (choice, howMuch) => {
+        switch (choice) {
+            case "PLN/EUR":
+                return howMuch * 0.21;
+            case "PLN/USD":
+                return howMuch * 0.22;
+            case "PLN/GBP":
+                return howMuch * 0.18;
+            case "PLN/CHF":
+                return howMuch * 0.21;
+            case "EUR/PLN":
+                return howMuch * 4.7;
+            case "USD/PLN":
+                return howMuch * 4.48;
+            case "GBP/PLN":
+                return howMuch * 5.45;
+            case "CHF/PLN":
+                return howMuch * 4.75;
+        }
     }
 
-    resultElement.innerText = `Po wymianie otrzymasz ${result.toFixed(2)} ${waluta}`;
+    const currencyResult = (choice) => {
+        switch (choice) {
+            case "PLN/EUR":
+                return "EUR";
+            case "PLN/USD":
+                return "USD";
+            case "PLN/GBP":
+                return "GBP";
+            case "PLN/CHF":
+                return "CHF";
+            case "EUR/PLN":
+                return "PLN";
+            case "USD/PLN":
+                return "PLN";
+            case "GBP/PLN":
+                return "PLN";
+            case "CHF/PLN":
+                return "PLN";
+        }
+    }
 
-});
+    const init = () => {
+        const numberElement = document.querySelector(".js-recalculate");
 
-numberElement.addEventListener("reset", (_event) => {
-    resultElement.innerText = "";
-});
+        numberElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const howMuchElement = document.querySelector(".js-howMuch");
+            const choiceElement = document.querySelector(".js-choice");
+            let resultElement = document.querySelector(".js-result");
+
+            const choice = choiceElement.value;
+            const howMuch = howMuchElement.value;
+            let result = calculateResult(choice, howMuch)
+            let waluta = currencyResult(choice)
+            resultElement.innerText = `Po wymianie otrzymasz ${result.toFixed(2)} ${waluta}`;
+        });
+
+        numberElement.addEventListener("reset", (_event) => {
+            resultElement.innerText = "";
+        });
+    }
+
+    init();
+}
